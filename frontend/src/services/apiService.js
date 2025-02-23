7a7d2ba577a7983eedeadf3a81a5b97bd503ff8a
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2025-02-23 22:44:56 +0530

    push code

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -4,10 +4,23 @@ const API_URL = 'https://techclawlegal.onrender.com/api';
 
 export const register = async (userData) => axios.post(`${API_URL}/auth/register`, userData);
 export const login = async (credentials) => axios.post(`${API_URL}/auth/login`, credentials);
-export const submitResignation = async (resignationData, token) => 
-    axios.post(`${API_URL}/resignation/submit`, resignationData, {
+// export const submitResignation = async (resignationData, token) => 
+//     axios.post(`${API_URL}/resignation/submit`, resignationData, {
+//         headers: { Authorization: `Bearer ${token}` }
+//     });
+
+export const submitResignation = async (resignationData, token) => {
+    if (!token) {
+        console.error('❌ Token is missing');
+        return;
+    }
+    
+    return axios.post(`${API_URL}/resignation/submit`, resignationData, {
         headers: { Authorization: `Bearer ${token}` }
     });
+};
+    
+    
 
 export const submitExitInterview = async (interviewData, token) => 
     axios.post(`${API_URL}/employee/exit-interview`, interviewData, {
